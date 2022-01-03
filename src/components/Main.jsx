536da63359ae616aa39881e6ae915ac7112ee57e
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-03 14:33:23 +1100

    components

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const name = "Pieter-Jan Delbecke"
+
+const Main = () => {
+    return(
+        <main>
+            <h1>Hello World! My name is {name} </h1>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto aspernatur vel quasi id nihil atque dignissimos corporis cum! Alias, commodi!</p>
+        </main>
+    )
+}
+
+export default Main
