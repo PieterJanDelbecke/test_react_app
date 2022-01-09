4fb4ccc6a1ff1823ef905169a983c9740db1cb8e
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-09 16:12:07 +1100

    added css file for styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const users = [
   {
     email: "h@gmail.com",
@@ -17,6 +18,13 @@ class App extends React.Component {
     return(
       <div className='container'>
         <h2>Login</h2>
+        <form className="login-form">
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' id='email' />
+          <label htmlFor='password'>Password</label>
+          <input type="password" name="password" id="password" />
+          <input type='submit' value="Submit" id="submit" />
+        </form>
       </div>
     )
   }
